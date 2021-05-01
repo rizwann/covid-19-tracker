e367c4b53704bd3ab279eb8b62a18ae454764c30
rizwann <rizwankabirsizan@gmail.com>
2021-05-01 23:36:11 +0200

    Initial Setup

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Cards({data}) {
+    console.log(data)
+    return (
+        <div>
+           <h1>Cards</h1> 
+        </div>
+    )
+}
+
+export default Cards
